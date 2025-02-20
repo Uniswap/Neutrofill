62410fd5f6d0a14875978a03821188e0baaa7319
0age <37939117+0age@users.noreply.github.com>
2025-02-20 14:06:37 -0500

    improve scripts

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-    },
-    build: {
-        outDir: path.resolve(__dirname, '../server/static'),
-        emptyOutDir: true,
-    },
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: resolve(__dirname, "../../server/static"),
+    emptyOutDir: true,
+  },
 });
